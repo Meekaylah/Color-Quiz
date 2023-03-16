8a4355f779f6ad02c3266f02f003983539a982df
Meekaylah <84780375+Meekaylah@users.noreply.github.com>
2023-03-16 12:58:29 +0100

    Randomize Hexadecimal Numbers

--- javasvript/script.js
+++ javasvript/script.js
@@ -2,5 +2,10 @@ const colors = document.querySelectorAll('.color-box h2');
 
 
 function generateColors() {
-	colors.forEach(color) => 
+	colors.forEach((color) => {
+		let hexCode = Math.random().toString(16);
+		console.log(hexCode)
+	});
 }
+
+generateColors();
