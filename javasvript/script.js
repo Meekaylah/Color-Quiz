8cc1a0b3c6697ec6467d35a87fbff24e403df6f3
Meekaylah <84780375+Meekaylah@users.noreply.github.com>
2023-03-18 21:06:55 +0100

    Update to Color Swatches

--- javasvript/script.js
+++ javasvript/script.js
@@ -1,11 +1,32 @@
+const newColor = [0, 1]
 const colors = document.querySelectorAll('.color-box h2');
+console.log(colors)
 
 
 function generateColors() {
-	colors.forEach((color) => {
+	
+	const arrColor = []
+	newColor.forEach((color) => {
 		let hexCode = '#' + Math.random().toString(16).substring(2,8);
-		color.style.backgroundColor = hexCode
+		//color.style.backgroundColor = hexCode
+		arrColor.push(hexCode)
+		console.log(hexCode)
 	});
+	console.log(arrColor)
+	const sample = [0, 1, 2]
+	const shuffledArray = sample.sort(() => 0.5 - Math.random());
+	const result = shuffledArray.slice(0, 3);
+	console.log(result)
+	colors[result[0]].style.backgroundColor = arrColor[0]
+	colors[result[2]].style.backgroundColor = arrColor[0]
+	colors[result[1]].style.backgroundColor = arrColor[1]
 }
+// function generateColors() {
+// 	colors.forEach((color) => {
+// 		let hexCode = '#' + Math.random().toString(16).substring(2,8);
+// 		color.style.backgroundColor = hexCode
+// 		console.log(hexCode)
+// 	});
+// }
 
 generateColors();
