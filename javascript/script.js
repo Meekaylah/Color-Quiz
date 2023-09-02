fbbdd577606edbff5a6fefe0b6dac4ee2c15363e
Meekaylah <84780375+Meekaylah@users.noreply.github.com>
2023-09-02 01:12:30 +0100

    Final changes

--- javascript/script.js
+++ javascript/script.js
@@ -1,13 +1,18 @@
+// Variables
+const options = document.querySelectorAll('.color-box h2');
+let oddColorIndex;
+let timerActive = true; // Flag to track if the timer is active
+let countdown = 20; // Initial countdown time in seconds
+let score = 0;
+let timer;
+
+// Function to get a random color in 'rgb(r, g, b)' format
 function getRandomColor() {
     const [red, green, blue] = Array.from({ length: 3 }, () => Math.floor(Math.random() * 256));
     return `rgb(${red}, ${green}, ${blue})`;
 }
 
-const options = document.querySelectorAll('.color-option');
-let oddColorIndex;
-let timerActive = true; // Flag to track if the timer is active
-let countdown = 20; // Initial countdown time in seconds
-
+// Function to assign random colors to options
 function assignRandomColors() {
     if (!timerActive) {
         // If the timer is not active, return without assigning new colors
@@ -20,7 +25,7 @@ function assignRandomColors() {
 
     randomColors[oddColorIndex] = getRandomColor(); // Generate a unique odd color
 
-    const oneColor = getRandomColor()
+    const oneColor = getRandomColor();
 
     options.forEach((option, index) => {
         if (randomColors[index] === undefined) {
@@ -29,22 +34,24 @@ function assignRandomColors() {
         }
 
         option.style.backgroundColor = randomColors[index];
-        
+
         if (index === oddColorIndex) {
             option.dataset.odd = true; // Mark the odd color
         } else {
             option.dataset.odd = false;
         }
     });
+
+    // Attach event listener for option clicks when the page loads
+    options.forEach(option => {
+    option.addEventListener('click', handleClick);
+});
 }
 
-let score = 0;
-
+// Function to update the timer display
 function updateTimerDisplay() {
-    // Update the timer display in the 'timer' element
     const timerElement = document.querySelector('.timer');
 
-    // Format the countdown time as "M:SS" (minutes:seconds)
     const minutes = Math.floor(countdown / 60);
     const seconds = countdown % 60;
     const formattedTime = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
@@ -52,27 +59,24 @@ function updateTimerDisplay() {
     timerElement.textContent = formattedTime;
 }
 
-let timer;
+// Function to start the timer
 function startTimer() {
     timerActive = true;
-
-    // Update the timer display immediately
     updateTimerDisplay();
 
     timer = setInterval(() => {
         countdown--;
 
-        // Update the timer display every second
         updateTimerDisplay();
 
         if (countdown <= 0) {
             timerActive = false;
             clearInterval(timer);
-
-            // After the timer expires, remove click event listeners
             options.forEach(option => {
                 option.removeEventListener('click', handleClick);
             });
+
+            showPopup(score);
         }
     }, 1000); // Update every 1000 milliseconds (1 second)
 }
@@ -80,26 +84,43 @@ function startTimer() {
 // Event handler for div clicks
 function handleClick(event) {
     if (!timerActive) {
-        // If the timer is not active, return without processing the click
         return;
     }
 
     const clickedOption = event.target;
     if (clickedOption.dataset.odd === 'true') {
-        // User clicked the correct odd color
         score++;
     }
     console.log("score:", score);
 
-    // After user's selection, generate new colors for the next round
     assignRandomColors();
 }
 
-options.forEach(option => {
-    option.addEventListener('click', handleClick);
+// Function to show the popup with the user's score
+function showPopup(score) {
+    const popup = document.getElementById('popup');
+    const scoreElement = document.getElementById('score');
+
+    scoreElement.textContent = score;
+    popup.style.display = 'block';
+}
+
+// Function to hide the popup
+function hidePopup() {
+    const popup = document.getElementById('popup');
+    popup.style.display = 'none';
+}
+
+// Event listener for the "New Game" button in the popup
+document.getElementById('new-game-btn').addEventListener('click', () => {
+    score = 0;
+    hidePopup();
+    countdown = 20;
+    updateTimerDisplay();
+    startTimer();
+    assignRandomColors();
 });
 
 // Start the timer when the page loads
 startTimer();
-
 assignRandomColors();
